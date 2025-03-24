e5717b08d3e800500e13e45428fbc4db9c80b6af
Tathya <coding.tathya@gmail.com>
2025-03-24 22:04:36 +0530

    perf: Made blog html be rendered when blog post is created rather than when resource is requested

--- blog/server/main.py
+++ blog/server/main.py
@@ -29,10 +29,12 @@ async def lifespan(_: FastAPI):
             slug TEXT PRIMARY KEY,
             title TEXT,
             content TEXT,
+            html TEXT,
+            tree TEXT,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             banner_url TEXT
         )''')
-        cur.execute('CREATE TABLE IF NOT EXISTS tag (name TEXT PRIMARY KEY)')
+        cur.execute('CREATE TABLE IF NOT EXISTS tag (name TEXT PRIMARY KEY, color TEXT)')
         cur.execute('CREATE TABLE IF NOT EXISTS blog_tag (blog_slug TEXT, tag_name TEXT, PRIMARY KEY (blog_slug, tag_name))')
         conn.commit()
 
@@ -41,6 +43,69 @@ async def lifespan(_: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 api = FastAPI()
+
+
+def make_html_from_blog(title: str, markdown: str) -> tuple[str, str]:
+    tree = f'''<h1>
+        <a href="#head">{title}</a>
+    </h1>'''
+
+    final_content = f'''<div class="head-container">
+        <h1>{title}</h1>
+        <a class="anchor" name="head"></a>
+    </div>'''
+
+    ol = False
+    ul = False
+
+    figure_count = 0
+
+    for i, line in enumerate(markdown.split('\n')):
+        if line.startswith('#'):
+            if ol:
+                final_content += '</ol>'
+                ol = False
+            elif ul:
+                final_content += '</ul>'
+                ul = False
+
+            hashes = len(line.split(' ')[0])
+            tree += f'''<h{hashes + 1}>
+                <a href=\"#h{i}\">{line.strip('#')}</a>
+            </h{hashes + 1}>'''
+
+            final_content += f'''<div class="head-container">
+                <h{hashes + 1}>{line.strip("#")}</h{hashes + 1}>
+                <a class="anchor" name="h{i}"></a>
+            </div>'''
+        else:
+            if not ol and not ul:
+                if line.startswith('1.'):
+                    final_content += '<ol>'
+                    ol = True
+                elif line.startswith('-'):
+                    final_content += '<ul>'
+                    ul = True
+
+            if ol and re.match(r'\d+\.', line):
+                line_content = re.sub(r'\d+\.', '', line)
+                final_content += f'<li>{line_content}</li>'
+            elif ul and line.startswith('-'):
+                final_content += f'<li>{line.strip("- ")}</li>'
+            elif (m := re.match(r'!\[(.*)\]\((.*)\)', line)):
+                figure_count += 1
+                final_content += f'''<figure>
+                    <img src="{m.group(2)}" alt="{m.group(1)}">
+                    <figcaption>
+                        <i>Figure {figure_count}:</i>
+                        {m.group(1)}
+                    </figcaption>
+                </figure>'''
+            else:
+                result = re.sub(r'\[(.*)\]\((.*)\)', r'<a href="\2">\1</a>', line)
+                final_content += f'<p>{result}</p>'
+
+    return tree, final_content
 
 
 @app.get('/')
@@ -53,79 +118,19 @@ async def blog(slug: str):
     template = open(BLOG).read()
     with sqlite3.connect(DATABASE) as conn:
         cur = conn.cursor()
-        cur.execute('SELECT title, content, created_at FROM blog WHERE slug = ?', (slug,))
+        cur.execute('''SELECT title, html, tree, created_at FROM blog WHERE slug = ?''', (slug,))
         res = cur.fetchone()
 
         if not res:
             return FileResponse(NOT_FOUND)
 
-        title, content, created_at = res
+        title, html, tree, created_at = res
         cur.execute('SELECT tag_name FROM blog_tag WHERE blog_slug = ?', (slug,))
         tags = [tag[0] for tag in cur.fetchall()]
 
-        tree = f'''<h1>
-            <a href="#head">{title}</a>
-        </h1>'''
-
-        final_content = f'''<div class="head-container">
-            <h1>{title}</h1>
-            <a class="anchor" name="head"></a>
-        </div>'''
-
-        ol = False
-        ul = False
-
-        figure_count = 0
-
-        for i, line in enumerate(content.split('\n')):
-            if line.startswith('#'):
-                if ol:
-                    final_content += '</ol>'
-                    ol = False
-                elif ul:
-                    final_content += '</ul>'
-                    ul = False
-
-                hashes = len(line.split(' ')[0])
-                tree += f'''<h{hashes + 1}>
-                    <a href=\"#h{i}\">{line.strip('#')}</a>
-                </h{hashes + 1}>'''
-
-                final_content += f'''<div class="head-container">
-                    <h{hashes + 1}>{line.strip("#")}</h{hashes + 1}>
-                    <a class="anchor" name="h{i}"></a>
-                </div>'''
-            else:
-                if not ol and not ul:
-                    if line.startswith('1.'):
-                        final_content += '<ol>'
-                        ol = True
-                    elif line.startswith('-'):
-                        final_content += '<ul>'
-                        ul = True
-
-                if ol and re.match(r'\d+\.', line):
-                    line_content = re.sub(r'\d+\.', '', line)
-                    final_content += f'<li>{line_content}</li>'
-                elif ul and line.startswith('-'):
-                    final_content += f'<li>{line.strip("- ")}</li>'
-                elif (m := re.match(r'!\[(.*)\]\((.*)\)', line)):
-                    figure_count += 1
-                    final_content += f'''<figure>
-                        <img src="{m.group(2)}" alt="{m.group(1)}">
-                        <figcaption>
-                            <i>Figure {figure_count}:</i>
-                            {m.group(1)}
-                        </figcaption>
-                    </figure>'''
-                else:
-                    result = re.sub(r'\[(.*)\]\((.*)\)', r'<a href="\2">\1</a>', line)
-                    final_content += f'<p>{result}</p>'
-
-
         return HTMLResponse(template.format(
             title=title,
-            content=final_content,
+            content=html,
             created_at=created_at,
             tags=', '.join(tags),
             tree=tree
@@ -165,7 +170,8 @@ async def post_blog_ep(
     with sqlite3.connect(DATABASE) as conn:
         cur = conn.cursor()
         try:
-            cur.execute('INSERT INTO blog (slug, title, content, banner_url) VALUES (?, ?, ?, ?)', (slug, title, content, banner_url))
+            tree, html = make_html_from_blog(title, content)
+            cur.execute('INSERT INTO blog (slug, title, content, html, tree, banner_url) VALUES (?, ?, ?, ?, ?, ?)', (slug, title, content, html, tree, banner_url))
         except sqlite3.IntegrityError:
             return status.HTTP_409_CONFLICT
 
