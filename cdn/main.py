27a2a990aee11c52f40f63ceb29251ce3fe0c6ea
Tathya <coding.tathya@gmail.com>
2025-04-11 22:08:58 +0530

    feat: Added endpoint to upload files

--- cdn/main.py
+++ cdn/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.staticfiles import StaticFiles
 import os
+import shutil
 
 if not os.path.exists("data"):
     os.makedirs("data")
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="data"), name="static")
+app.mount("/retrieve", StaticFiles(directory="data"), name="static")
+
+@app.post("/upload")
+async def upload(file: UploadFile = File(...)):
+    with open(f"data/{file.filename}", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {"filename": file.filename}
 
