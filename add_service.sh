97bbe27debf17949622cd6baabd95470adc67669
Tathya <coding.tathya@gmail.com>
2025-04-14 18:32:16 +0530

    chore: Added shell script to add services to my cf config

---
+++ add_service.sh
@@ -0,0 +1,20 @@
+NAME=$1
+PORT=$2
+
+TUNNEL_NAME="${TUNNEL_NAME:-arson-dev}"
+DOMAIN="${DOMAIN:-arson.dev}"
+CLOUDFLARED_CONFIG_FILE="${CLOUDFLARED_CONFIG_FILE:-/etc/cloudflared/config.yml}"
+
+if [ -z "$NAME" ] || [ -z "$PORT" ]; then
+  echo "Usage: $0 <service-name> <port>"
+  exit 1
+fi
+
+cloudflared tunnel route dns add $TUNNEL_NAME $NAME.$DOMAIN
+
+echo "Creating tunnel for $NAME on port $PORT..."
+
+cat <<EOF >>$CLOUDFLARED_CONFIG_FILE
+    - hostname: $NAME.$DOMAIN
+      service: http://localhost:$PORT
+EOF
