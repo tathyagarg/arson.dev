5d558d0f1e9cbe351adaca02eaf22e2f43f77a58
Tathya <coding.tathya@gmail.com>
2025-05-16 04:45:35 +0530

    refactor(1/): Starting refactor of archive to sveltekit

---
+++ archive/new/src/lib/components/navbar.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { updateTheme } from "$lib";
+
+  onMount(() => {
+    const theme = localStorage.getItem("theme");
+    const toggle = document.getElementById("toggle") as HTMLInputElement;
+
+    toggle.checked = theme === "light";
+
+    toggle.addEventListener("change", (e: Event) => {
+      const target = e.target as HTMLInputElement;
+      localStorage.setItem("theme", target.checked ? "light" : "dark");
+      updateTheme(localStorage.getItem("theme") ?? "dark");
+    });
+  });
+</script>
+
+<div id="navbar">
+  <div class="flex items-center absolute">
+    <span class="text-2xl m-2">🌙</span>
+    <label class="relative inline-block w-[60px] h-[34px]">
+      <input type="checkbox" class="peer hidden" id="toggle" />
+      <span
+        class="absolute cursor-pointer top-0 left-0 right-0 bottom-0 bg-(--surface0) duration-300 rounded-[34px] before:absolute before:content-[''] before:h-[26px] before:w-[26px] before:left-[4px] before:bottom-[4px] before:bg-(--text) before:duration-300 before:rounded-full peer-checked:before:translate-x-[26px]"
+      ></span>
+    </label>
+    <span class="text-2xl m-2">☀️</span>
+  </div>
+  <nav class="bg-(--mantle) w-full flex">
+    <ul class="mt-[1em] mb-[1em] flex flex-row justify-around w-full list-none">
+      <li><a href="/" class="text-(--text)! no-underline">Home</a></li>
+      <li><a href="/sites" class="text-(--text)! no-underline">Sites</a></li>
+    </ul>
+  </nav>
+</div>
---
+++ archive/new/src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import "../app.css";
+  import { onMount } from "svelte";
+  import { updateTheme } from "$lib";
+
+  import Navbar from "$lib/components/navbar.svelte";
+  import "$lib/styles/base.css";
+
+  let { children } = $props();
+
+  onMount(() => {
+    let theme = localStorage.getItem("theme") ?? "dark";
+    if (theme == null) {
+      localStorage.setItem("theme", "dark");
+    }
+    updateTheme(theme);
+  });
+</script>
+
+<Navbar />
+{@render children()}
+
+<style>
+</style>
