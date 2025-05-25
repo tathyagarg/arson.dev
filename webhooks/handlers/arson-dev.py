fbbb17ca592c789032c43623e785623f4be4f6e0
Tathya <coding.tathya@gmail.com>
2025-05-25 19:53:12 +0530

    feat: Added logging to webhooks

--- webhooks/handlers/arson-dev.py
+++ webhooks/handlers/arson-dev.py
@@ -1,15 +1,26 @@
 from fastapi import Request
+import subprocess
 import os
+from pathlib import Path
 
-HOME = '/home/tathya'
+HOME = Path.home()
+LOGFILE = HOME / 'logs' / 'arson-dev.log'
 
-def redeploy_sveltekit(name: str):
-    os.system(f'cd {HOME}/{name} && npm run build && pm2 restart {name}')
+def redeploy_service_factory(steps: list[str], log_file_path: Path = LOGFILE):
+    def redeploy_service(name: str):
+        for i, step in enumerate(steps, 1):
+            with open(log_file_path, 'a') as log_file:
+                log_file.write(f'Step {i} for {name}: {step}\n')
+
+                status = subprocess.run(step.format(name=name), shell=True, cwd=HOME / 'arson.dev' / name, stdout=log_file, stderr=log_file).returncode
+
+                log_file.write(f'Step {i} for {name} completed with status {status}\n')
+
+    return redeploy_service
 
 
-def redeploy_fastapi(name: str):
-    os.system(f"systemctl --user restart {name}")
-
+redeploy_sveltekit = redeploy_service_factory(['npm i', 'npm run build', 'pm2 restart {name'])
+redeploy_fastapi = redeploy_service_factory(['source ../.venv/bin/activate', 'pip install -r requirements.txt', 'systemctl --user restart {name}'])
 
 SERVICES = {
     'archive': redeploy_sveltekit,
@@ -22,7 +33,9 @@ SERVICES = {
 async def handler(request: Request):
     data = await request.json()
 
-    os.system('git pull')
+    with open(LOGFILE, 'a') as log_file:
+        log_file.write(f"Received data: {data}\n")
+        subprocess.run("git pull", shell=True, cwd=HOME, stdout=log_file, stderr=log_file)
 
     changed_files = []
 
