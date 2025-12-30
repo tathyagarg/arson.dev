8d489e35b5ccacc6ff9184ebe57754e3cf59e688
Tathya <coding.tathya@gmail.com>
2025-12-30 20:49:23 +0530

    feat: New portfolio

---
+++ me/src/routes/work/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Card from "$lib/components/Card.svelte";
+</script>
+
+<div class="pt-12">
+  <h2>3</h2>
+  <h1>Work</h1>
+</div>
+
+<div class="grid grid-cols-1 gap-4 pt-8">
+  <Card
+    title="DWMUN'25"
+    content="A website for my school's Model United Nations conference."
+    bannerUrl="/screenshots/dwmun.png"
+    link="https://dwmun.co.in"
+  />
+
+  <Card
+    title="Techfusion'24"
+    content="Website for an online tech event hosted by my school."
+    bannerUrl="/screenshots/techfusion.png"
+    link="https://archive.arson.dev/sites/techfusion"
+  />
+
+  <Card
+    title="Intoxicode"
+    content="Website for a custom programming language I made"
+    bannerUrl="/screenshots/intoxicode.png"
+    link="https://intoxicode.arson.dev/"
+  />
+</div>
