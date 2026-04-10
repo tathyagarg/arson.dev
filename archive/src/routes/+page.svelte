dd0dd6284f73cf81a9d9646393f6141095c2b9e2
Tathya <coding.tathya@gmail.com>
2026-04-11 04:46:39 +0530

    shit

--- archive/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<main>
-  <div id="card">
-    <h1 class="font-black">Archive</h1>
-    <p>
-      This website holds my archived websites (and blog articles in the future!)
-    </p>
-  </div>
-</main>
