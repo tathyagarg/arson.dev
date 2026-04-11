e45a4501cd8b029e106ee5ecdfaaae5b9cb88c56
Tathya <coding.tathya@gmail.com>
2026-04-11 22:23:41 +0530

    feat: 3BP

--- me/src/routes/projects/+page.svelte
+++ me/src/routes/projects/+page.svelte
@@ -11,20 +11,6 @@
       emph: true,
     },
     {
-      name: "DWMUN'25",
-      description: "A website for the DWMUN'25 conference",
-      img: "/assets/projects/dwmun.png",
-      link: "https://dwmun.co.in/",
-      github: "https://github.com/tathyagarg/dwmun25",
-    },
-    {
-      name: "Techfusion'24",
-      description: "A website for an online tech fest organized by my school",
-      img: "/assets/projects/techfusion.png",
-      link: "https://archive.arson.dev/sites/techfusion/",
-      github: null,
-    },
-    {
       name: "Intoxicode",
       description: "A programming language that I wrote from scratch in Zig",
       img: "/assets/projects/intoxicode.png",
@@ -39,6 +25,28 @@
       link: "https://github.com/tathyagarg/odingine",
       github: "https://github.com/tathyagarg/odingine",
     },
+    {
+      name: "3-body-problem Simulation",
+      description:
+        "A simple simulation of the 3-body problem, written in Zig with Raylib for rendering",
+      img: "/assets/projects/3bp.png",
+      linnk: null,
+      github: "https://github.com/tathyagarg/3-body-problem",
+    },
+    {
+      name: "DWMUN'25",
+      description: "A website for the DWMUN'25 conference",
+      img: "/assets/projects/dwmun.png",
+      link: "https://dwmun.co.in/",
+      github: "https://github.com/tathyagarg/dwmun25",
+    },
+    {
+      name: "Techfusion'24",
+      description: "A website for an online tech fest organized by my school",
+      img: "/assets/projects/techfusion.png",
+      link: "https://archive.arson.dev/sites/techfusion/",
+      github: null,
+    },
   ];
 </script>
 
@@ -51,12 +59,19 @@
       "
     >
       <img src={project.img} alt={project.name} class="w-full" />
-      <a href={project.link} class="flex flex-col items-center no-underline!">
+      {#if project.link}
+        <a href={project.link} class="flex flex-col items-center no-underline!">
+          <div class="w-full px-2">
+            <h3 class="text-lg font-bold">{project.name}</h3>
+            <p>{project.description}</p>
+          </div>
+        </a>
+      {:else}
         <div class="w-full px-2">
           <h3 class="text-lg font-bold">{project.name}</h3>
           <p>{project.description}</p>
         </div>
-      </a>
+      {/if}
 
       {#if project.github}
         <div class="flex items-end justify-start mt-2 px-2 pb-2">
